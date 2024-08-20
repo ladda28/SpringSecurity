6d933ca94107243820c0316c369c4c75127f1967
ladda28 <bunnyladda4416@gmail.com>
2024-08-20 23:19:42 +0530

    JWT

--- src/main/java/com/SpringDemo/SpringSecurity/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.SpringDemo.SpringSecurity.Repo;
-
-import com.SpringDemo.SpringSecurity.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-    User findByUserName(String username);
-}
